c8e651a47e87977fbe2b5418dfa39ef172f96e14
caleb taylor <calebs.phone2017@gmail.com>
2026-07-14 20:07:54 -0400

    added icons to the cards, can now make custom icons for the website using any color

--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,35 @@
-class mead_card extends HTMLElement {
-  connectedCallback() {
+class MeadCard extends HTMLElement {
+    connectedCallback() {
 
-    const type = this.getAttribute("type") || "traditional";
-    const name = this.getAttribute("name") || "blank";
-    this.innerHTML = `
-		<div class="card ${type}"> 
-			<h1>${name}</h1>
-			<p> this is the cards information</p>
-		</div>
-    `;
-  }
+        const type = this.getAttribute("type") || "";
+        const name = this.getAttribute("name") || "Blank";
+        const icon = this.getAttribute("icon") || "";
+
+        if(icon == ""){
+            this.innerHTML = `
+            <div class="card ${type}">
+
+                <h1>${name}</h1>
+
+                <p>This is the card's information.</p>
+            </div>
+        `;
+        }
+        else{
+            this.innerHTML = `
+            <div class="card ${type}">
+
+                <h1>${name}</h1>
+
+                <img class="card-icon" src="${icon}" alt="${name} icon">
+
+                <p>This is the card's information.</p>
+            </div>
+        `;
+        }
+
+        
+    }
 }
-customElements.define("mead-card", mead_card);
 
-console.log("js loaded ")
+customElements.define("mead-card", MeadCard);
